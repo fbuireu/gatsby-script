e4304979d17b932e923c7780b373aa6b6ff96c1c
Verner Keel <verner.keel@gmail.com>
2021-09-28 13:20:58 +0300

    Added more content to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ const IndexPage = () => (
     <h1 style={{ marginBottom: '20px' }}>
       Testing third party script's perfomance
     </h1>
+    <p>The chat widget is in the bottom left corner</p>
     <p>This is Nimbata dynamic tracking number:</p>
     <button>0161 794 0088</button>
     <p style={{ marginTop: `20px` }}>This is Trustbox:</p>

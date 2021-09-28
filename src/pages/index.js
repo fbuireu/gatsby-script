a0ad22f2d4efea215e55941c1f896c865699b6bd
Verner Keel <verner.keel@gmail.com>
2021-09-28 10:19:04 +0300

    Added phone nr button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
-const IndexPage = () => <p>Index</p>;
+const IndexPage = () => (
+  <button style={{ margin: `50px` }}>0161 794 0088</button>
+);
 
 export default IndexPage;

27951b5850184f5ec30984d5147c113d1d6d2e85
Verner Keel <verner.keel@gmail.com>
2021-09-28 10:08:29 +0300

    Added User.com chat widget script via SSR API

---
+++ gatsby-ssr.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { v4 } from 'uuid';
+
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      key={v4()}
+      async={true}
+      defer={true}
+      data-cfasync={false}
+      type={`text/javascript`}
+      dangerouslySetInnerHTML={{
+        __html: `
+            window.civchat = {
+              apiKey: "PJCwx7",
+            };`,
+      }}
+    />,
+    <script
+      key={v4()}
+      async={true}
+      defer={true}
+      data-cfasync={false}
+      type={`text/javascript`}
+      src={`https://mms.user.com/widget.js`}
+    />,
+  ]);
+};

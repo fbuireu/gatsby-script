5271f0c34479f8e3b83467fc58a653bdfd49cb9d
Verner Keel <verner.keel@gmail.com>
2021-09-28 11:23:19 +0300

    Amended style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 
 const IndexPage = () => (
-  <section>
-    <h1>Testing third party script's perfomance</h1>
-    <h2>This is Nimbata dynamic trackign number</h2>
-    <button style={{ margin: `50px` }}>0161 794 0088</button>
+  <section style={{ margin: '50px' }}>
+    <h1 style={{ marginBottom: '20px' }}>
+      Testing third party script's perfomance
+    </h1>
+    <p>This is Nimbata dynamic tracking number:</p>
+    <button>0161 794 0088</button>
   </section>
 );
 

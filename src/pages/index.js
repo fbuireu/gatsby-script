fca2ab683d9cdd342783de35edc6d233f94f6acd
Verner Keel <verner.keel@gmail.com>
2021-09-28 10:28:38 +0300

    Added descriptive content to indexpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import * as React from 'react';
 
 const IndexPage = () => (
-  <button style={{ margin: `50px` }}>0161 794 0088</button>
+  <section>
+    <h1>Testing third party script's perfomance</h1>
+    <h2>This is Nimbata dynamic trackign number</h2>
+    <button style={{ margin: `50px` }}>0161 794 0088</button>
+  </section>
 );
 
 export default IndexPage;

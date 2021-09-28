457e48050f7dee3eae39b273f861e041a8b22f08
Verner Keel <verner.keel@gmail.com>
2021-09-28 10:04:15 +0300

    Added GUA/ADS tracking scripts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  plugins: [`gatsby-plugin-gatsby-cloud`],
+  plugins: [
+    `gatsby-plugin-gatsby-cloud`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          process.env.GOOGLE_ADS_TRACKING_ID,
+          process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        ],
+        pluginConfig: {
+          head: false,
+        },
+      },
+    },
+  ],
 };
